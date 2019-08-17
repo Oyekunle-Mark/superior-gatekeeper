e5e28972e4ede9cc3d5daf9101b2e10e033f9408
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-17 14:30:37 +0100

    Refactored the mailer helper

--- helper/mailer.js
+++ helper/mailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 
+// turn on less secure app on the google settings before running this block
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -8,14 +9,17 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const mailOptions = {
-  from: 'admin@where-to-code.com',
-  to: 'oyekunlemac@yahoo.com',
-  subject: 'Hope it works',
-  html: '<strong>Yipeee!!!!</strong>',
-};
+module.exports = receiverMail => {
+  const mailOptions = {
+    from: 'admin@where-to-code.com',
+    to: receiverMail,
+    subject: 'Welcome to Where to Code.',
+    html:
+      '<strong>Your Where to Code account was successfully created.</strong>',
+  };
 
-transporter.sendMail(mailOptions, (err, info) => {
-  if (err) console.log(err);
-  else console.log(info);
-});
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+};
